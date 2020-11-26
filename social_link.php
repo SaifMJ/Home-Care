8d539c60eb0bf2d09bd05488bb4f1256093f7cb6
Saif <saifmjmj17@gmail.com>
2020-11-26 18:07:46 +0600

    Home-Care

---
+++ social_link.php
@@ -0,0 +1,47 @@
+<style>
+        .social-icon-bar {
+    z-index: 1111111;
+    position: fixed;
+    top: 49%;
+    right: 0px;
+}
+.social-icon-bar a {
+    display: block;
+    text-align: center;
+    padding: 16px;
+    transition: all 0.3s ease;
+    color: white;
+    font-size: 20px;
+}
+.facebook {
+    background: #3B5998;
+    color: white;
+}
+.twitter {
+    background: #29b200;
+    color: white;
+}
+.google {
+    background: #dd4b39;
+    color: white;
+}
+.linkedin {
+    background: #007bb5;
+    color: white;
+}
+.youtube {
+    background: #bb0000;
+    color: white;
+}
+.website {
+    background: #9400D3;
+    color: white;
+}
+    </style>
+        <div class="social-icon-bar">
+  <a href="https://www.facebook.com/mohammadsaifulalam.saif/" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a> 
+  <a href="https://wa.me/8801754307898" class="twitter" target="_blank"><i class="fa fa-whatsapp"></i></a> 
+  <a href="https://www.youtube.com/channel/UCY-1tOG0Saur-vT7VerOByg?view_as=subscriber" class="youtube" target="_blank"><i class="fa fa-youtube"></i></a> 
+    <a href="https://saifulalamcv.000webhostapp.com/" class="website" target="_blank"><i class="fa fa-external-link"></i></a> 
+
+</div>
